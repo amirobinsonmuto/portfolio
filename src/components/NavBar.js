f7057fd064bd055e041419551cca5c70ef694534
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-06-08 14:56:37 -0700

    Update styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,102 +1,157 @@
 import React from "react";
-import { useState } from "react";
-import Contact from "./Contact";
+import { useState, useEffect } from "react";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
 // import { IoMdMenu } from "react-icons/io";
 
 export default function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
   const handleClick = () => {
-    if (!isOpen) {
-      setIsOpen(true);
+    if (!isMobileMenuOpen) {
+      setIsMobileMenuOpen(true);
     } else {
-      setIsOpen(false);
+      setIsMobileMenuOpen(false);
     }
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const shouldCollapse = window.scrollY > 0;
+      setIsCollapsed(shouldCollapse);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="top-0 fixed w-full py-3 bg-white shadow z-50">
-      <div className="container mx-auto flex items-center justify-between">
-        {/* logo*/}
-        <a
-          className="text-red-500 text-xl font-bold leading-relaxed inline-block py-2 uppercase"
-          href="/"
-        >
-          Ruby Jam
-        </a>
-
-        {/* menu links */}
-        <div className="hidden md:flex gap-5 items-center bg-white">
-          <a href="A" className=" text-gray-800 hover:text-primary">
-            About Us
+    <nav className="w-full z-40 bg-secondary">
+      <div
+        className={`${
+          isCollapsed ? "justify-between fixed right-0 left-0" : "justify-end"
+        } hidden md:flex bg-secondary items-center mx-auto py-2 z-50 md:px-48 shadow`}
+      >
+        <div className={`${isCollapsed ? "block" : "hidden"}`}>
+          <a className="font-bold uppercase text-white" href="/">
+            Ami Robinson
           </a>
-          <a href="A" className=" text-gray-800 hover:text-primary">
-            Services
-          </a>
-          <Contact className="text-gray-800 hover:text-primary" showEnvelope={false}/>
         </div>
-
-        {/* Hamburger button */}
-        <button
-          className="hamburger block md:hidden text-primary focus:ring-2 focus:ring-inset focus:ring-white"
-          aria-controls="primary-navigation"
-          aria-expanded={isOpen}
-          onClick={handleClick}
+        <ul
+          className={`${
+            isCollapsed ? "flex" : "hidden"
+          } gap-5 items-center text-white`}
         >
-          <svg
-            className="hamburger fill-primary"
-            viewBox="0 0 100 100"
-            width="50"
-            height="50"
-            transform="scale(0.8)"
+          <li>
+            <a href="A" className="hover:text-primary">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="A" className="hover:text-primary">
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href="A" className="hover:text-primary">
+              Contact
+            </a>
+          </li>
+        </ul>
+        <div className=" bg-secondary flex items-center justify-end gap-5 text-white">
+          <FaGithub size={24} color="white" />
+          <FaLinkedinIn size={24} color="white" />
+        </div>
+      </div>
+      <div className="w-full bg-white py-5">
+        <div className="w-full bg-white container mx-auto flex items-center justify-between text-gray-800">
+          <a
+            className="text-xl font-bold leading-relaxed inline-block py-2 uppercase"
+            href="/"
           >
-            <rect
-              className="line top"
-              width="80"
-              height="10"
-              x="10"
-              y="25"
-              rx="5"
-            ></rect>
-            <rect
-              className="line middle"
-              width="80"
-              height="10"
-              x="10"
-              y="45"
-              rx="5"
-            ></rect>
-            <rect
-              className="line bottom"
-              width="80"
-              height="10"
-              x="10"
-              y="65"
-              rx="5"
-            ></rect>
-          </svg>
-        </button>
+            Ami Robinson
+          </a>
+
+          {/* menu links */}
+          <ul className="hidden md:flex gap-5 items-center">
+            <li>
+              <a href="A" className="hover:text-primary">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="A" className="hover:text-primary">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="A" className="hover:text-primary">
+                Contact
+              </a>
+            </li>
+          </ul>
+
+          {/* Hamburger button */}
+          <button
+            className="hamburger block md:hidden text-secondary focus:ring-2 focus:ring-inset focus:ring-white"
+            aria-controls="primary-navigation"
+            aria-expanded={isMobileMenuOpen}
+            onClick={handleClick}
+          >
+            <svg
+              className="hamburger fill-secondary"
+              viewBox="0 0 100 100"
+              width="50"
+              height="50"
+              transform="scale(0.8)"
+            >
+              <rect
+                className="line top"
+                width="80"
+                height="10"
+                x="10"
+                y="25"
+                rx="5"
+              ></rect>
+              <rect
+                className="line middle"
+                width="80"
+                height="10"
+                x="10"
+                y="45"
+                rx="5"
+              ></rect>
+              <rect
+                className="line bottom"
+                width="80"
+                height="10"
+                x="10"
+                y="65"
+                rx="5"
+              ></rect>
+            </svg>
+          </button>
+        </div>
       </div>
       {/* mobile menu */}
       <div
         className={` 
         absolute md:hidden top-15 right-0 w-40 bg-gray-800 bg-opacity-90 transition-transform duration-300 ease-in-out transform-gpu p-4 rounded-md flex flex-col items-end ${
-          isOpen ? "mobile-menu-open" : "mobile-menu-closed"
+          isMobileMenuOpen ? "mobile-menu-open" : "mobile-menu-closed"
         }`}
       >
-        <a
-          href="a"
-          className="block text-white hover:text-primary mb-2"
-        >
-          About Us
+        <a href="a" className="block text-white hover:text-primary mb-2">
+          About
         </a>
-        <a
-          href="a"
-          className="block text-white hover:text-primary mb-2"
-        >
-          Services
+        <a href="a" className="block text-white hover:text-primary mb-2">
+          Projects
         </a>
-        <Contact className="block text-white hover:text-primary" showEnvelope={false}/>
+        <a href="A" className="block text-white hover:text-primary mb-2">
+          Contact
+        </a>
       </div>
     </nav>
   );
