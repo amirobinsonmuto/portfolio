ac4bbc12f5e7dbb717a7c53fb40b26a5dc9fedea
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-06-13 11:21:21 -0700

    Update styling

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,26 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FaGithub } from "react-icons/fa";
 import { LuExternalLink } from "react-icons/lu";
 
-const Project = ({ project }) => {
+const Project = ({ index, project }) => {
+  const [imageSrc, setImageSrc] = useState("");
+
+  useEffect(() => {
+    const loadImage = async () => {
+      try {
+        const importedImage = await import(`../assets/${project.imagePath}`);
+        setImageSrc(importedImage.default);
+      } catch (error) {
+        // Handle error loading the image
+        console.error("Error loading image:", error);
+      }
+    };
+
+    loadImage();
+  }, [project.imagePath]);
+
   return (
-    <div>
-      <div>
-        <p3>{project.title}</p3>
-        <p>{project.description}</p>
+    <div className="grid grid-cols-2 p-12 gap-24 ">
+      <div className="col-span-1">
+        <p3 className="text-4xl">{project.title}</p3>
+        <p className="my-6">{project.description}</p>
         <p>
           {project.technologies.map((tech) => (
             <span>{tech} </span>
           ))}
         </p>
-        <a href={project.gitHubLink}>
-          <FaGithub size={24} color="black" />
-        </a>
-        <a href={project.liveLink}>
-          <LuExternalLink size={24} color="black" />
-        </a>
+        <div className="flex gap-2 my-3">
+          <a href={project.gitHubLink}>
+            <FaGithub size={24} color="black" />
+          </a>
+          <a href={project.liveLink}>
+            <LuExternalLink size={24} color="black" />
+          </a>
+        </div>
       </div>
-      <img src={`../assets/${project.imagePath}`} alt={project.title} />
+      <div
+        className={`col-span-1 relative ${
+          index % 2 === 0 ? "order-first" : ""
+        }`}
+      >
+        {imageSrc && (
+          <img
+            src={imageSrc}
+            className="border-8 border-pink relative"
+            alt={project.title}
+          />
+        )}
+      </div>
     </div>
   );
 };
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,8 +5,36 @@ const Projects = () => {
   const projects = [
     {
       title: "React Shopping Site",
-      description: "lalalal",
-      technologies: ["react", "JavaScript"],
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+      technologies: ["React", "JavaScript"],
+      gitHubLink: "https://github.com/amirobinsonmuto/e-commerce",
+      liveLink: "https://amirobinsonmuto.github.io/e-commerce/",
+      imagePath: "sample-project.png",
+    },
+    {
+      title: "React Shopping Site",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+      technologies: ["React", "JavaScript"],
+      gitHubLink: "https://github.com/amirobinsonmuto/e-commerce",
+      liveLink: "https://amirobinsonmuto.github.io/e-commerce/",
+      imagePath: "sample-project.png",
+    },
+    {
+      title: "React Shopping Site",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+      technologies: ["React", "JavaScript"],
+      gitHubLink: "https://github.com/amirobinsonmuto/e-commerce",
+      liveLink: "https://amirobinsonmuto.github.io/e-commerce/",
+      imagePath: "sample-project.png",
+    },
+    {
+      title: "React Shopping Site",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+      technologies: ["React", "JavaScript"],
       gitHubLink: "https://github.com/amirobinsonmuto/e-commerce",
       liveLink: "https://amirobinsonmuto.github.io/e-commerce/",
       imagePath: "sample-project.png",
@@ -16,7 +44,7 @@ const Projects = () => {
     <section className="max-w-7xl mx-auto mt-12 mb-48">
       <h2 className="text-gray-800 h2">Projects</h2>
       {projects.map((project, index) => (
-        <Project ket={index} project={project} />
+        <Project key={index} project={project} index={index} />
       ))}
     </section>
   );
