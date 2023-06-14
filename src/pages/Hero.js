4f1f6d4d478918442ed92b31d450fbebf0f13707
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-06-14 09:44:46 -0700

    Update styling

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -14,12 +14,12 @@ const Hero = () => {
     <header className="header bg-primary">
       <div className="container mx-auto items-center flex custom-height bg-primary gap-5">
         <div ref={ref} className="mx-4 md:pr-8 animation-hidden">
+          <h1 className="z-50 font-semibold text-6xl text-white font-secondary tracking-widest uppercase">
+            Ami Robinson{" "}
+          </h1>
           <h2 className="text-fontPink text-8xl font-primary md:absolute -top-16 left-64">
             Front End Developer
           </h2>
-          <h3 className="z-50 font-semibold text-6xl text-white font-secondary tracking-widest uppercase">
-            Ami Robinson{" "}
-          </h3>
 
           <p className="mt-8 text-xl leading-relaxed text-white font-secondary">
             I specialize in building and maintaining websites and full-stack
