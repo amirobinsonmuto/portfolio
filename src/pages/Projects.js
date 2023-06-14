045eb5ee28f9b6b38dd9a5de9fe47cee2e281d71
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-06-14 09:08:24 -0700

    Update styling

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -25,7 +25,7 @@ const Projects = () => {
       title: "React Shopping Site",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-      technologies: ["React", "JavaScript"],
+      technologies: ["React", "JavaScript", "CSS"],
       gitHubLink: "https://github.com/amirobinsonmuto/e-commerce",
       liveLink: "https://amirobinsonmuto.github.io/e-commerce/",
       imagePath: "sample-project.png",
